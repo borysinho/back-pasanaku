b268123ea4f1f1f0c4be42192aef702fdfac1fcf
borysinho <borysquiroga@gmail.com>
2024-04-14 21:44:40 -0400

    updating_Estado_Notificacion State

--- prisma/migrations/20240415011905_updated/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "EstadoNotificacion" ADD VALUE 'NoEnviado';
-
--- AlterTable
-ALTER TABLE "Invitados_Juegos" ALTER COLUMN "estado_notificacion_correo" SET DEFAULT 'NoEnviado',
-ALTER COLUMN "estado_notificacion_whatsapp" SET DEFAULT 'NoEnviado';
